a26f7bdbe54637aec79aa2364b75c7ae707f0a8a
RyanErik <ryaneriklimadasilva95@gmail.com>
2025-07-20 17:23:30 -0300

    fix: cards

--- load-card.js
+++
@@ -1,8 +0,0 @@
-fetch('cards.html')
-  .then(response => response.text())
-  .then(html => {
-    document.getElementById('cardsGrupo').innerHTML = html;
-  })
-  .catch(err => {
-    console.error('Erro ao carregar os cards:', err);
-  });
