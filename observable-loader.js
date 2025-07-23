60e575c5430730b19566ce0712753103d242a11f
RyanErik <ryaneriklimadasilva95@gmail.com>
2025-07-22 22:47:49 -0300

    versao-final

--- observable-loader.js
+++ observable-loader.js
@@ -1,7 +1,9 @@
 import { Runtime, Inspector } from "https://cdn.jsdelivr.net/npm/@observablehq/runtime@5/dist/runtime.js";
 import define from "https://api.observablehq.com/d/fbb99c3b8ad509ba.js?v=4";
+import define2 from "https://api.observablehq.com/d/5cbb28335f38f36f.js?v=4";
 
 const runtime = new Runtime();
+const runtime2 = new Runtime();
 
 runtime.module(define, name => {
   if (name === "viewof chartGrupo") return new Inspector(document.querySelector("#observablehq-viewof-chartGrupo-cf89fb02"));
@@ -12,12 +14,17 @@ runtime.module(define, name => {
   return ["selected","grupoSelecionado","mapaBiomasFormatado","selectedBioma","biomaSelecionado","biomaSelecionadoNorm","biomasNaoSelecionados","biomaDestacado","camadaColorida","camadaCinza"].includes(name);
 });
 
+runtime2.module(define2, name => {
+  if (name === "grafico") return new Inspector(document.querySelector("#observablehq-grafico-dbb91d51"));
+});
+
 window.addEventListener("load", () => {
   const containers = [
     "#observablehq-viewof-chartGrupo-cf89fb02",
     "#observablehq-mapa-cf89fb02",
     "#observablehq-viewof-chartBolhas-cf89fb02",
-    "#observablehq-mapaBioma-cf89fb02"
+    "#observablehq-mapaBioma-cf89fb02",
+    "#observablehq-grafico-dbb91d51"  
   ];
   containers.forEach(selector => {
     const container = document.querySelector(selector);
